c3586d9677c3092279abae409f8bfb116c640a15
tplal <tplalaettin@gmail.com>
2022-06-24 21:35:02 +0300

    wallpaper

--- lib/screen/details/details_page.dart
+++ lib/screen/details/details_page.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-//import 'package:flutter/services.dart';
+import 'package:flutter/services.dart';
 //import 'package:wallpaper_manager/wallpaper_manager.dart';
 
 class DetailsPage extends StatefulWidget {
@@ -10,21 +12,44 @@ class DetailsPage extends StatefulWidget {
 }
 
 class _DetailsPageState extends State<DetailsPage> {
-  /*String _wallpaperAsset = 'Unknown';
-  String _wallpaperAssetWithCrop = 'Unknown';
+/*  String? _platformVersion = 'Unknown';
+  String? _wallpaperFile = 'Unknown';
+  String? _wallpaperFileWithCrop = 'Unknown';
+  String? _wallpaperAsset = 'Unknown';
+  String? _wallpaperAssetWithCrop = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    try {
+      platformVersion = await WallpaperManager.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
 
   Future<void> setWallpaperFromAsset() async {
     setState(() {
       _wallpaperAsset = "Loading";
     });
     String result;
-    String assetPath = widget.imgPath;
+    String assetPath = "assets/tmp1.jpg";
     try {
       result = await WallpaperManager.setWallpaperFromAsset(
           assetPath, WallpaperManager.HOME_SCREEN);
     } on PlatformException {
       result = 'Failed to get wallpaper.';
     }
+
     if (!mounted) return;
 
     setState(() {
@@ -37,8 +62,7 @@ class _DetailsPageState extends State<DetailsPage> {
       _wallpaperAssetWithCrop = "Loading";
     });
     String result;
-    String assetPath = widget.imgPath;
-
+    String assetPath = "assets/tmp1.jpg";
     try {
       result = await WallpaperManager.setWallpaperFromAssetWithCrop(
           assetPath, WallpaperManager.HOME_SCREEN, 0, 0, 800, 800);
@@ -51,8 +75,8 @@ class _DetailsPageState extends State<DetailsPage> {
     setState(() {
       _wallpaperAssetWithCrop = result;
     });
-  }*/
-
+  }
+*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,21 +91,32 @@ class _DetailsPageState extends State<DetailsPage> {
             fit: BoxFit.cover,
           )),
           child: Center(
-            child: Column(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // setWallpaperFromAsset();
-                  },
-                  child: const Text("Uygula"),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // setWallpaperFromAssetWithCrop();
-                  },
-                  child: const Text("Uygula"),
-                ),
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                children: const <Widget>[
+                  /*  RaisedButton(
+                    child: Text("Platform Version"),
+                    onPressed: initPlatformState,
+                  ),
+                  Center(
+                    child: Text('Running on: $_platformVersion\n'),
+                  ),
+                  RaisedButton(
+                    child: Text("Set wallpaper from asset"),
+                    onPressed: setWallpaperFromAsset,
+                  ),
+                  Center(
+                    child: Text('Wallpaper status: $_wallpaperAsset\n'),
+                  ),
+                  RaisedButton(
+                    child: Text("Set wallpaper from asset with crop"),
+                    onPressed: setWallpaperFromAssetWithCrop,
+                  ),
+                  Center(
+                    child: Text('Wallpaper status: $_wallpaperAssetWithCrop\n'),
+                  ),*/
+                ],
+              ),
             ),
           ),
         ),
